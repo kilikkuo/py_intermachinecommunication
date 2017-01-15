c5903944846e638f360a5c7f3b6051f307fb4c8e
Kilik Kuo <kikuo@mozilla.com>
2017-01-15 19:35:41 +0800

    Make this module be able to be packaged by providing a setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='simple_host_target',
+      version='0.1',
+      description='A simple host / target server to execute task and deliver result',
+      url='https://github.com/kilikkuo/py_simple_host_target.git',
+      author='Kilik Kuo',
+      author_email='kilik.kuo@gmail.com',
+      license='MIT',
+      packages=['simple_host_target'],
+      zip_safe=False)
---
+++ simple_host_target/README.md
@@ -0,0 +1,13 @@
+# Provide simple host / target module to be setup easily (i assume ...) in
+  different machine.
+
+# Terminal 1
+  $> python3 host.py
+
+# Terminal 2
+  $> python3 target.py
+
+# Terminal 3
+  $> python3 sample_project.py
+  then
+  press s + <enter>
