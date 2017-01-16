107ac7a56583fe5ca2dbb73d3a6141b6a95af7b3
Kilik Kuo <kikuo@mozilla.com>
2017-01-16 16:57:55 +0800

    Make it possible to config host / target IP to Target.

--- simple_host_target/__init__.py
+++ simple_host_target/__init__.py
@@ -2,5 +2,5 @@ from .definition import ResultWrapper, ExecutorWrapper,\
                         HOST_PIPEIN_NAME, HOST_PIPEOUT_NAME, get_local_IP
 
 from .host import create_host
-from .target import launch_target
+from .target import create_target
 from .sample_project import test_sample_project
--- simple_host_target/target.py
+++ simple_host_target/target.py
@@ -42,38 +42,52 @@ def launch_process(cb_to_target, wrapper, parent_conn, child_conn):
         cb_to_target(result)
 
 class ExecutionTarget(object):
-    def __init__(self, host_ip, localhost = None):
-        # max_client should always be 1 (TaskHost)
-        localhost = localhost if localhost else get_local_IP()
-        self.host_ip = host_ip
-        print(" Create target ... @(%s), host@(%s) "%(localhost, host_ip))
-        self.server = Server(ip = localhost, port = TARGET_PORT, max_client = 1)
+    def __init__(self, target_IP):
+        self.host_IP = ""
+        self.target_IP = target_IP
         self.parent_conn, self.child_conn = Pipe()
         pass
 
-    def shutdown(self):
+    def __shutdown(self):
         if self.server:
             self.server.shutdown()
 
-    def run_until_exception(self):
+    def __ensure_host_IP(self, IP):
+        if IP == "":
+            print("Empty host IP !, Please enter a valid Host IP ...")
+            try:
+                for line in sys.stdin:
+                    processed_IP = line.strip()
+                    break
+                return processed_IP
+            except:
+                print("Something wrong while processing host IP, exit !")
+                sys.exit(1)
+        return IP
+
+    def run_until_exception(self, host_IP):
+        self.host_IP = self.__ensure_host_IP(host_IP)
+        # TODO: max_client should always be 1 (TaskHost)
+        self.server = Server(ip = self.target_IP, port = TARGET_PORT, max_client = 1)
         self.server.run_server(self.__task_package_callback)
         try:
             # CAUTION : Using sys.stdin will result in hang while spawning
             #           process.
+            print("Target is running ...")
             import time
             while 1:
                 time.sleep(1)
         except:
             print("[Target][Exception] while lining in ")
         finally:
-            self.shutdown()
+            self.__shutdown()
         pass
 
     def __recv_from_executor(self, serialized_result_wrapper):
         print("[Target][P] result : %s "%(str(serialized_result_wrapper)))
         c = None
         try:
-            c = Client(ip = self.host_ip, port = HOST_PORT)
+            c = Client(ip = self.host_IP, port = HOST_PORT)
             c.send_data(serialized_result_wrapper)
         except:
             traceback.print_exc()
@@ -107,15 +121,22 @@ class ExecutionTarget(object):
                 thread = None
 
 # Exported function
-def launch_target(host_ip):
-    h_ip = host_ip.strip()
-    target = ExecutionTarget(h_ip)
-    target.run_until_exception()
+def create_target():
+    target_ip = get_local_IP()
+    print("Creating target @(%s) ... are you sure ? Enter Yes/No."%(target_ip))
+    try:
+        for line in sys.stdin:
+            msg = line.lower().strip()
+            if msg.find('yes') >= 0:
+                return ExecutionTarget(target_ip)
+            else:
+                break
+    except:
+        traceback.print_exc()
+    print("\r\nNothing created")
+    return None
 
 if __name__ == "__main__":
-    print("Please enter HOST IP : ")
-    host_ip = None
-    for line in sys.stdin:
-        host_ip = line
-        break
-    launch_target(host_ip)
+    target = create_target()
+    if target:
+        target.run_until_exception("")
